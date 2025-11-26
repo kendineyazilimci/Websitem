b980bffd41c7129f996376dd5be7c5016475a565
Ozan Güneysu <ozanguneysu6@gmail.com>
2025-11-27 00:46:17 +0300

    Bitti

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const path = require("path");
+const fs = require("fs"); 
 
 const navbarLinks = [
     {id: 2, name: "İletişim", url: "/contact"},
@@ -9,42 +9,20 @@ const navbarLinks = [
     {id: 4, name: "Neredeyim?", url: "/whereami"}
 ];
 
-router.get("/contact", (req, res) => { res.render('users/contact', { navbarLinks }); });
-
-router.post("/contact", async (req, res) => {
-    const { name, email, subject, message } = req.body;
-    console.log("--> Outlook ile gönderim deneniyor...");
-
-    try {
-        const transporter = nodemailer.createTransport({
-            host: "smtp-mail.outlook.com",
-            port: 587,
-            secure: false,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS  
-            },
-            tls: {
-                ciphers: 'SSLv3',
-                rejectUnauthorized: false
-            }
-        });
-
-        const mailOptions = {
-            from: process.env.EMAIL_USER, 
-            to: process.env.EMAIL_USER,
-            subject: `Siteden Mesaj: ${subject}`,
-            text: `Gönderen: ${name} (${email})\n\nMesaj:\n${message}`
-        };
-
-        let info = await transporter.sendMail(mailOptions);
-        console.log("--> BAŞARILI! ID: " + info.messageId);
-        
-        res.send(`<script>alert("Gönderildi!"); window.location.href = "/contact";</script>`);
-
-    } catch (error) {
-        console.error("HATA:", error);
-        res.send(`<script>alert("Hata: ${error.message}"); window.location.href = "/contact";</script>`);
-    }
+router.get("/", (req, res) => {
+    res.render('users/homepage.ejs', { navbarLinks }); 
 });
+
+router.get("/contact", (req, res) => {
+    res.render('users/contact', { navbarLinks });
+});
+
+router.get("/about", (req, res) => { 
+    res.render('users/about', { navbarLinks }); 
+});
+
+router.get("/whereami", (req, res) => {
+    res.render('users/whereami.ejs', { navbarLinks });
+});
+
 module.exports = router;
